0c3f8fecf0aae338bf19eee9f5e18fc0ad96e7dc
unknown <lauty_2000@hotmail.com>
2023-09-25 13:11:47 -0300

    Revert "inciso i-A"
    This reverts commit f788c2da208a1a23dc343bab5ad0364899fc6a58.

--- Feature-1.java
+++ Feature-1.java
@@ -3,6 +3,5 @@ public class EjemploIngDeSofware
      public static void main (String args[])
      {
         System.out.print("Feature-1 branch");
-        System.out.print("Modificacion");
      }
 }
