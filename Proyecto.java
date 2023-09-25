a68de398473c64617e23dc31765b9b18e3bd8a94
unknown <lauty_2000@hotmail.com>
2023-09-25 12:29:53 -0300

    inciso a

--- Proyecto.java
+++ Proyecto.java
@@ -3,5 +3,6 @@ public class EjemploIngDeSofware
      public static void main (String args[])
      {
         System.out.print("Master branch");
+        System.out.print("Modificacion inciso a");
      }
 }
