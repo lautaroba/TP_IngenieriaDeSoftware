5760f6ad20ee12c60922a81033f0d87afdf181e7
unknown <lauty_2000@hotmail.com>
2023-09-25 21:49:36 -0300

    inciso d

--- Proyecto.java
+++ Proyecto.java
@@ -4,5 +4,6 @@ public class EjemploIngDeSofware
      {
         System.out.print("Master branch");
         System.out.print("Modificacion inciso a");
+        System.out.print("Release 1 lista");
      }
 }
